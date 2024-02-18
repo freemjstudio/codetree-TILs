012588848b05d041f1d97489ec563bc13318b557
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-02-18 19:41:13 +0900

    Title: 행복한 수열의 개수, Time: 310ms, Memory: 25MB, Status: Passed - CodeTree

--- 240218/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240218/행복한 수열의 개수/number-of-happy-sequence.py
@@ -9,23 +9,26 @@ answer = 0
 
 for i in range(n): # 가로 
     count = 1 
-    for j in range(n-1):
-        if arr[i][j] == arr[i][j+1]:
+    max_count = 1
+    for j in range(1, n):
+        if arr[i][j-1] == arr[i][j]:
             count += 1
         else:
             count = 1
-        
-    if count >= m:
+        max_count = max(max_count, count)
+    if max_count >= m:
         answer += 1
 
 for i in range(n):
     count = 1 
-    for j in range(n-1):
-        if arr[j][i] == arr[j+1][i]:
+    max_count = 1
+    for j in range(1, n):
+        if arr[j-1][i] == arr[j][i]:
             count += 1
         else: 
             count = 1
-    if count >= m:
+        max_count = max(count, max_count)
+    if max_count >= m:
         answer += 1
     
 
