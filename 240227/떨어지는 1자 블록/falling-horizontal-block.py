b1117d46e351d397de7a1e86074a1c06a63d71d0
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-02-27 19:59:25 +0900

    Title: 떨어지는 1자 블록, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240227/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240227/떨어지는 1자 블록/falling-horizontal-block.py
@@ -4,19 +4,19 @@ grid = []
 for _ in range(n):
     grid.append(list(map(int, input().split())))
 
-delta = (1, 0) # 아래쪽으로 이동 
 
 # block 삽입하기 
 k = k-1 
 block_pos = [] # block 을 이루는 좌표 값 
 
 max_row = 0 # 가장 낮게 내려갈 수 있는 열의 위치 찾기 
-for row in range(n-1):
-    for col in range(k, k+m-1):
+for row in range(n):
+    for col in range(k, k+m):
         if grid[row][col] == 1: 
+            max_row = row -1 
             break 
-    max_row = row  
 max_row -= 1
+# 최종 block 위치 표시하기 
 for col in range(k, k+m):
     grid[max_row][col] = 1
 
