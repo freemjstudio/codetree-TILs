580e47d86fc9a7a6b5b61a0a568edcd71c721739
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-02-27 19:53:46 +0900

    Title: 떨어지는 1자 블록, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240227/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,5 +1,4 @@
 n, m, k = map(int, input().split())
-# block -> 1, empty -> 0 
 
 grid = []
 for _ in range(n):
@@ -7,40 +6,21 @@ for _ in range(n):
 
 delta = (1, 0) # 아래쪽으로 이동 
 
-# 1. block 삽입하기 
+# block 삽입하기 
 k = k-1 
 block_pos = [] # block 을 이루는 좌표 값 
-for i in range(m):
-    grid[k][i] = 1 # new block 
-    block_pos.append((k, i))
 
-# 2. block 객체 이동시키기 
-while True:
-    flag = True 
-    new_block_pos = []
-    for x, y in block_pos:
-        nx, ny = x+1, y 
-        if 0 <= nx < n and 0 <= ny < n:
-            if grid[nx][ny] == 0: 
-                new_block_pos.append((nx, ny))
-            else:
-                flag = False 
-        else: 
-            flag = False 
-
-    if not flag:
-        break 
-    
-    # remove prev position 
-    for x, y in block_pos:
-        grid[x][y] = 0
-    # move to new position 
-    for x, y in new_block_pos:
-        grid[x][y] = 1
-    # update
-    block_pos = new_block_pos
-
-# 3. answer 
+max_row = 0 # 가장 낮게 내려갈 수 있는 열의 위치 찾기 
+for row in range(n):
+    for col in range(k, k+m-1):
+        if grid[row][col] == 1: 
+            break 
+    max_row = row  
+max_row -= 1
+for col in range(k, k+m):
+    grid[max_row][col] = 1
+    # print(max_row, col)
+# answer 
 for i in range(n):
     for j in range(n):
         print(grid[i][j], end=" ")
