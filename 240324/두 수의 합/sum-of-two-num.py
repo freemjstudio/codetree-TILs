cd3fbf4efa678366b39ae07aa23cf580d2c445dd
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-03-24 12:26:45 +0900

    Title: 두 수의 합, Time: 208ms, Memory: 36MB, Status: Passed - CodeTree

--- 240324/두 수의 합/sum-of-two-num.py
+++ 240324/두 수의 합/sum-of-two-num.py
@@ -1,16 +1,20 @@
-from collections import defaultdict 
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-numbers = defaultdict(list)
-for idx, elem in enumerate(arr):
-    numbers[elem].append(idx) # 위치 
+numbers = dict()
 
 answer = 0 
 
-for key, value in numbers.items():
-    tmp = (k - key)
-    if tmp in numbers:
-        answer += len(numbers[tmp])
+for elem in arr: 
+    diff = (k - elem)
+
+    # 가능한 쌍의 수 
+    if diff in numbers:
+        answer += numbers[diff]
+    if elem in numbers:
+        numbers[elem] += 1
+    else: 
+        numbers[elem] = 1 # initlaize 
+
 
 print(answer)
