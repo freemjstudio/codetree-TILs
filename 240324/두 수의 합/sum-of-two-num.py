ffdcdad9a386568381c09ad4f16059966acba222
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-03-24 11:39:34 +0900

    Title: 두 수의 합, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240324/두 수의 합/sum-of-two-num.py
+++ 240324/두 수의 합/sum-of-two-num.py
@@ -2,12 +2,14 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 answer = 0 
+numbers = {}
+for idx, elem in enumerate(arr):
+    numbers[elem] = idx # 위치 
 
-for i in range(len(arr)):
-    for j in range(len(arr)):
-        if i == j:
-            continue
-        if arr[i] + arr[j] == k:
+for i in range(len(arr)): 
+    tmp = (k - arr[i])
+    if tmp in numbers:
+        if i != numbers[tmp]:
             answer += 1
 
 print(answer//2)
