f9b4af9a876be2a5fddef4e023232386165459a8
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-03-30 09:50:41 +0900

    Title: 1차원 바람, Time: 179ms, Memory: 26MB, Status: Passed - CodeTree

--- 240330/1차원 바람/The-1D-wind-blows.py
+++ 240330/1차원 바람/The-1D-wind-blows.py
@@ -1,21 +1,22 @@
 n, m, q = map(int, input().split()) # n * m 
-arr = []
+arr = [[0] * (m+1) for _ in range(n+1)]
 
 SHIFT_RIGHT = 0
 SHIFT_LEFT = 1
 
-for _ in range(n):
+for row in range(1, n+1):
     line = list(map(int, input().split()))
-    arr.append(line)
+    for col, num in enumerate(line, start = 1):
+        arr[row][col] = num
 
 def shift(row, cur_dir):
     if cur_dir == SHIFT_RIGHT: # 오른쪽으로 밀어야 하는 경우 
-        arr[row].insert(0, arr[row].pop())
+        arr[row].insert(1, arr[row].pop())
     else: 
-        arr[row].insert(m-1, arr[row].pop(0))
+        arr[row].insert(m, arr[row].pop(1))
 
 def check_same_num(row1, row2):
-    for col in range(m):
+    for col in range(1, m+1):
         if arr[row1][col] == arr[row2][col]:
             return True 
     return False 
@@ -29,7 +30,6 @@ def simulate(start_row, start_dir):
 
     # 전파 시키기 - 위 방향 
     cur_dir = start_dir
-
     for row in range(start_row, 1, -1):
         if check_same_num(row, row-1):
             shift(row-1, cur_dir)
@@ -39,7 +39,7 @@ def simulate(start_row, start_dir):
 
     # 전파 시키기 - 아래 방향 
     cur_dir = start_dir
-    for row in range(start_row, n-1):
+    for row in range(start_row, n):
         if check_same_num(row, row+1):
             shift(row+1, cur_dir)
             cur_dir = flip_dir(cur_dir)
@@ -47,14 +47,14 @@ def simulate(start_row, start_dir):
             break 
 
 def print_result():
-    for row in range(n):
-        for col in range(m):
+    for row in range(1, n+1):
+        for col in range(1, m+1):
             print(arr[row][col], end=" ")
         print()
 
 for _ in range(q):
     row, wind_dir = input().split()
-    row = int(row) - 1
+    row = int(row)
     if wind_dir == 'L':
         cur_dir = SHIFT_RIGHT
     else:
