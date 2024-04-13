0efb8a896e994b163ac2777ef19aa1ed713dcc12
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-04-13 20:50:31 +0900

    Title: 나무박멸, Time: 399ms, Memory: 29MB, Status: Passed - CodeTree

--- 240413/나무박멸/tree-kill-all.py
+++ 240413/나무박멸/tree-kill-all.py
@@ -10,8 +10,8 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 # 대각선 이동 
-tx = [-1, 1, -1, 1]
-ty = [-1, 1, 1, -1]
+tx = [-1, 1, 1, -1]
+ty = [-1, -1, 1, 1]
 
 # 성장 
 def grow():
@@ -29,9 +29,7 @@ def grow():
 
 # 번식 - 번식 가능한 칸의 개수 만큼 번식됨 
 def spread(): 
-    global tree_pos
     temp = [[0] * n for _ in range(n)]
-
     for i in range(n):
         for j in range(n):
             if grid[i][j] <= 0: 
@@ -40,7 +38,7 @@ def spread():
             cnt = 0 
             for t in range(4):
                 nx, ny = i + dx[t], j + dy[t]
-                if 0 <= nx < n and 0 <= ny < n: 
+                if 0 <= nx < n and 0 <= ny < n:
                     if herb[nx][ny]:
                         continue
                     if grid[nx][ny] == 0:
@@ -48,15 +46,18 @@ def spread():
             # 번식 진행 
             for t in range(4):
                 nx, ny = i + dx[t], j + dy[t]
-                if 0 <= nx < n and 0 <= ny < n:
-                    if herb[nx][ny]:
-                        continue
-                    if grid[nx][ny] == 0:
-                        temp[nx][ny] += grid[nx][ny] // cnt 
+                if not (0 <= nx < n and 0 <= ny < n):
+                    continue 
+                if herb[nx][ny]:
+                    continue
+                if grid[nx][ny] == 0:
+                    temp[nx][ny] += grid[i][j] // cnt 
+
 
     for i in range(n):
         for j in range(n):
             grid[i][j] += temp[i][j]
+
 
 
 # 제초제 위치 구하기 
@@ -67,7 +68,7 @@ def get_kill_pos():
 
     for i in range(n):
         for j in range(n): 
-            if grid[i][j] <= 0: 
+            if grid[i][j] <= 0: # 나무가 있는 칸에 뿌려야 함 ! 
                 continue 
             kill_count = grid[i][j]
             for t in range(4):
@@ -75,35 +76,37 @@ def get_kill_pos():
                 for _ in range(k): # 대각선 방향 K 까지 영향줌 
                     nx += tx[t]
                     ny += ty[t]
-                    if 0 <= nx < n and 0 <= ny < n: 
-                        if grid[nx][ny] < 0:
-                            break 
-                        kill_count += grid[nx][ny]
-                    else: # 범위 벗어나면 확산 중단  
+                    if not (0 <= nx < n and 0 <= ny < n):
+                        break  
+                    if grid[nx][ny] <= 0: # 벽이 있거나 나무가 아예 없는 칸 
                         break 
+                    kill_count += grid[nx][ny]
+             
             if max_kill < kill_count:
                 max_kill = kill_count
                 r, c = i, j
-    
+
     answer += max_kill
 
     # 제초제 뿌리기 
     if grid[r][c] > 0:
         grid[r][c] = 0 
         herb[r][c] = C 
-
-        for t in range(4):
+        # 나무 있는 칸이므로 
+        for t in range(4): # 대각선 방향으로 전파 
             nx, ny = r, c 
             for _ in range(k):
                 nx, ny = nx + tx[t], ny + ty[t]
-                if 0 <= nx < n and 0 <= ny < n:
-                    if grid[nx][ny] < 0:
-                        break # 벽인 경우 
-                    if grid[nx][ny] == 0: # 나무 없는 경우 
-                        herb[nx][ny] = C
-                        break 
-                    grid[nx][ny] = 0 
+                if not (0 <= nx < n and 0 <= ny < n):
+                    break 
+                if grid[nx][ny] < 0: # 벽 
+                    break 
+                if grid[nx][ny] == 0: # 나무가 아예 없는 칸 
                     herb[nx][ny] = C 
+                    break 
+
+                grid[nx][ny] = 0 
+                herb[nx][ny] = C 
 
 
 def delete_herb():
@@ -115,7 +118,9 @@ def delete_herb():
 for _ in range(m):
     grow()
     spread()
-    get_kill_pos()
+    
     delete_herb() # 제초제 1년 감소 
+    get_kill_pos()
+    
 
 print(answer)
