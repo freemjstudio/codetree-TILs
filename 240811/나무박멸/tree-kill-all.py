9e30f5c2ad1e6c1fc371b1abb95d8d9abddcb0ca
Minji (Elena) Woo <41604678+freemjstudio@users.noreply.github.com>
2024-08-11 19:47:42 +0900

    Title: 나무박멸, Time: 339ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240811/나무박멸/tree-kill-all.py
+++ 240811/나무박멸/tree-kill-all.py
@@ -34,6 +34,7 @@ def growth():
     for x, y, amount in grow_pos:
         arr[x][y] += amount
 
+
 def breeding():
     breeding_pos = []  # 번식이 일어날 위치 기록
     for x in range(n):
@@ -50,9 +51,11 @@ def breeding():
                 if cnt > 0:
                     for nx, ny in new_pos:
                         breeding_pos.append((nx, ny, (arr[x][y] // cnt)))
+
     # update arr
     for x, y, amount in breeding_pos:
         arr[x][y] += amount
+
 
 # 가장 많이 박멸할 수 있는 칸 찾기
 def find_most_kill_pos():
@@ -88,9 +91,13 @@ def find_most_kill_pos():
 
 # 나무 박멸하기
 def kill_tree(x, y):
+    new_killer_pos = []  # 해당 년도에 새롭게 제초제를 뿌린 위치
+
+    # x, y = find_most_kill_pos()
     kill_cnt = arr[x][y]
-    killer_arr[x][y] = c+1
+    killer_arr[x][y] = c
     arr[x][y] = -2
+    new_killer_pos.append((x, y))
     # 박멸 실행하기
     for i in range(4):
         nx, ny = x, y
@@ -98,26 +105,36 @@ def kill_tree(x, y):
             nx, ny = nx + dx2[i], ny + dy2[i]
             if (0 <= nx < n and 0 <= ny < n):
                 if arr[nx][ny] == -1:  # 벽을 만나는 경우
-                    break
+                    break 
                     # 박멸한 나무 수 기록하기
+                if arr[nx][ny] == 0:
+                    killer_arr[nx][ny] = c
+                    arr[nx][ny] = -2
+                    break 
+
                 if arr[nx][ny] > 0:
                     kill_cnt += arr[nx][ny]
                     # 제초제 뿌렸음을 표시하기
                     arr[nx][ny] = -2
-                    killer_arr[nx][ny] = c+1
+                    killer_arr[nx][ny] = c
                     # 제초제 유효 기간
+                    new_killer_pos.append((nx, ny))
                 if arr[nx][ny] == -2:
-                    killer_arr[nx][ny] = c+1
-          
-    return kill_cnt
+                    killer_arr[nx][ny] = c
+                    new_killer_pos.append((nx, ny))
+
+    return kill_cnt, new_killer_pos
 
 
 # 제초제 1년 유효기간 반영 및 제초제 없애기
-def remove_killer():
+def remove_killer(new_killer_pos):
     for x in range(n):
         for y in range(n):
-            killer_arr[x][y] = max(0, killer_arr[x][y] - 1)
-                
+            if killer_arr[x][y] > 0 and (x, y) not in new_killer_pos:
+                killer_arr[x][y] -= 1
+                if killer_arr[x][y] == 0 and arr[x][y] == -2:
+                    arr[x][y] = 0  # 제초제 효과 없어짐
+
 
 def print_arr(a):
     for i in range(n):
@@ -134,11 +151,12 @@ for year in range(m):
     # print_arr(arr)
     x, y = find_most_kill_pos()
     if x >= 0 and y >= 0:
-        tree = kill_tree(x, y)  # 박멸한 나무 수
+        tree, new_killer_pos = kill_tree(x, y)  # 박멸한 나무 수
         answer += tree
     else:
-        break 
+        break
     # 새롭게 제초제를 뿌린 공간은 남은 년수를 remove_killer에서 빼면 안됨 !
-    remove_killer()
+
+    remove_killer(new_killer_pos)
 
 print(answer)
